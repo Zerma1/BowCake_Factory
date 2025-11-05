87e9e0a4e5e70c2dce237b2a6990b300f0dd0850
Zerma1 <58811114+Zerma1@users.noreply.github.com>
2025-11-05 16:18:29 +0100

    Add files via upload

---
+++ back-Blabla/UtilisateurRepository.java
@@ -0,0 +1,9 @@
+package cepn.defaut.repository;
+
+import cepn.defaut.models.Utilisateur;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
+}
